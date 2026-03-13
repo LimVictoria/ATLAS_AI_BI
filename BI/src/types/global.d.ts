6cb676cf92594abb763fe57ba2377b7841ad9bfc
LimVictoria <147840820+LimVictoria@users.noreply.github.com>
2026-03-13 10:09:02 +0800

    Update global.d.ts

--- BI/src/types/global.d.ts
+++ BI/src/types/global.d.ts
@@ -1,2 +1,26 @@
-declare module 'react-plotly.js'
+declare module 'react-plotly.js' {
+  import * as Plotly from 'plotly.js'
+  import * as React from 'react'
+
+  interface PlotParams {
+    data: Plotly.Data[]
+    layout?: Partial<Plotly.Layout>
+    config?: Partial<Plotly.Config>
+    style?: React.CSSProperties
+    className?: string
+    useResizeHandler?: boolean
+    onInitialized?: (figure: any, graphDiv: any) => void
+    onUpdate?: (figure: any, graphDiv: any) => void
+    onPurge?: (figure: any, graphDiv: any) => void
+    onError?: (err: any) => void
+    divId?: string
+    revision?: number
+    debug?: boolean
+    [key: string]: any
+  }
+
+  class Plot extends React.Component<PlotParams> {}
+  export default Plot
+}
+
 declare module 'plotly.js'
